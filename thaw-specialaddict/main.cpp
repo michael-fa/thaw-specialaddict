6ca19f1d3764453a9f88dfb8111adf99fece3dca
Michael Fa <michixx.bat@gmail.com>
2017-11-23 20:36:56 +0100

    initial commit

---
+++ thaw-specialaddict/main.cpp
@@ -0,0 +1,48 @@
+#include <windows.h>
+
+//Taken from thmp/utils.cpp - Thanks to th-mp!
+void UnFuck(DWORD addr, int size)
+{
+	DWORD d;
+	VirtualProtect((PVOID)addr, size, PAGE_EXECUTE_READWRITE, &d);
+}
+
+
+DWORD WINAPI Start(LPVOID lpParam)
+{
+	//Don't let the specialmeter increase slowly if no tricks/combo is happenin
+	//<<<< Taken from thmp/patches.cpp  - Thanks to th-mp! >>>>
+	UnFuck(0x56BEE3, 3);
+	memset((LPVOID)0x56BEE3, 0x90, 3);
+
+	//Don't reset specialmeter to zero if player is bailing/failing a trick/hitting a wall
+	//<<<< Taken from thmp/patches.cpp  - Thanks to th-mp! >>>>
+	UnFuck(0x56CE06, 3);
+	memset((LPVOID)0x56CE06, 0x90, 3);
+	//ExitThread(0);
+	return 1;
+}
+
+
+
+
+BOOL WINAPI DllMain(HINSTANCE module_handle, DWORD reason_for_call, LPVOID reserved)
+{
+	if (reason_for_call == DLL_PROCESS_ATTACH)
+	{
+		//Don't let the specialmeter increase slowly if no tricks/combo is happenin
+		//<<<< Taken from thmp/patches.cpp  - Thanks to th-mp! >>>>
+		UnFuck(0x56BEE3, 3);
+		memset((LPVOID)0x56BEE3, 0x90, 3);
+
+		//Don't reset specialmeter to zero if player is bailing/failing a trick/hitting a wall
+		//<<<< Taken from thmp/patches.cpp  - Thanks to th-mp! >>>>
+		UnFuck(0x56CE06, 3);
+		memset((LPVOID)0x56CE06, 0x90, 3);
+		//DisableThreadLibraryCalls(module_handle);
+		//CreateThread(0, 0, Start, module_handle, 0, 0); //New main thread, get the fuck out of freezing issues
+	}
+	if (reason_for_call == DLL_PROCESS_DETACH)
+	{
+	}
+}
