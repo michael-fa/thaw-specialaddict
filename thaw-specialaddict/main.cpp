5bac37961dbaa888278db089c4a163ac6ae7f556
lp_ <1826793+michael-fa@users.noreply.github.com>
2019-02-04 23:53:47 +0100

    Update main.cpp

--- thaw-specialaddict/main.cpp
+++ thaw-specialaddict/main.cpp
@@ -14,13 +14,9 @@ BOOL WINAPI DllMain(HINSTANCE module_handle, DWORD reason_for_call, LPVOID reser
 {
 	if (reason_for_call == DLL_PROCESS_ATTACH)
 	{
-		//Don't let the specialmeter increase slowly if no tricks/combo is happenin
-		//<<<< Taken from thmp/patches.cpp  - Thanks to th-mp! >>>>
 		UnFuck(0x56BEE3, 3);
 		memset((LPVOID)0x56BEE3, 0x90, 3);
 
-		//Don't reset specialmeter to zero if player is bailing/failing a trick/hitting a wall
-		//<<<< Taken from thmp/patches.cpp  - Thanks to th-mp! >>>>
 		UnFuck(0x56CE06, 3);
 		memset((LPVOID)0x56CE06, 0x90, 3);
 	}
